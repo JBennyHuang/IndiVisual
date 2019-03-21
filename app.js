fe35f9a643f0720087db8d1437893781d8b446e7
Shibe <hibenny2@gmail.com>
2019-03-20 21:54:25 -0400

    commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+var app = express();
+
+app.use(express.static('resources/html'));
+app.use(express.static('resources/stylesheet'));
+app.use(express.static('resources/javascript'));
+
+// bootstrap
+app.use(express.static('node_modules/bootstrap/dist'));
+
+// temp // will be moved to database
+app.use(express.static('resources/images'));
+
+app.get('/', function (req, res) {
+    res.sendFile('index.html');
+});
+
+app.listen(3000);
